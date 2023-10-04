b5696ab28cb9e19095f4748af5ad998b2786fc86
yevhenverkhalantsev <97880997+yevhenverkhalantsev@users.noreply.github.com>
2023-10-04 14:32:14 +0200

    initial commit

---
+++ oblig1_Yevhen_Verkhalantsev/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace oblig1_Yevhen_Verkhalantsev.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
